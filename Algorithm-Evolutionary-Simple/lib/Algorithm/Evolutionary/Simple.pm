b0404100a72f41957ab88c5074f71d1fbef03aa9
JJ Merelo <jj@merelo.net>
2011-11-04 10:08:03 +0100

    Added most functions

--- Algorithm-Evolutionary-Simple/lib/Algorithm/Evolutionary/Simple.pm
+++ Algorithm-Evolutionary-Simple/lib/Algorithm/Evolutionary/Simple.pm
@@ -5,10 +5,13 @@ use strict;
 use Carp;
 
 use version; our $VERSION = qv('0.0.3');
+use Carp qw(croak);
 
 use base 'Exporter';
+use Sort::Key::Top qw(rnkeytop) ;
 
-our @EXPORT_OK= qw( random_chromosome );
+our @EXPORT_OK= qw( random_chromosome max_ones spin get_pool_roulette_wheel 
+		    produce_offspring mutate crossover );
 
 
 # Module implementation here
@@ -19,6 +22,80 @@ sub random_chromosome {
     $string .= (rand >0.5)?1:0;
   }
   $string;
+}
+
+sub max_ones {
+  my $str=shift;
+  my $count = 0;
+  while ($str) {
+    $count += chop($str);
+  }
+  $count;
+}
+
+sub get_pool_roulette_wheel {
+  my $population = shift || croak "No population here";
+  my $fitness_of = shift || croak "need stuff evaluated";
+  my $need = shift || croak "I need to know the new population size";
+
+  my $total_fitness = 0;
+  map(  $total_fitness += $fitness_of->{$_} , @$population );
+  my @best = rnkeytop { $fitness_of->{$_} } 2 => @$population;
+  my @wheel = map( $fitness_of->{$_}/$total_fitness, @$population);
+  my @slots = spin( \@wheel, scalar(@$population) );
+  my @pool;
+  my $index = 0;
+  do {
+    my $p = $index++ % @slots;
+    my $copies = $slots[$p];
+    for (1..$copies) {
+      push @pool, $population->[$p];
+    }
+  } while ( @pool < $need );
+  
+  @pool;
+}
+
+sub spin {
+   my ( $wheel, $slots ) = @_;
+   my @slots = map( $_*$slots, @$wheel );
+   return @slots;
+}
+
+sub produce_offspring {
+  my $pool = shift || croak "Pool missing";
+  my $offspring_size = shift || croak "Population size needed";
+  my @population = ();
+  my $population_size = scalar( @$pool );
+  for ( my $i = 0; $i < $offspring_size/2; $i++ )  {
+    my $first = $pool->[rand($population_size)];
+    my $second = $pool->[rand($population_size)];
+    
+    push @population, crossover( $first, $second );
+  }
+  map( $_ = mutate($_), @population );
+  return @population;
+}
+
+sub mutate {
+  my $chromosome = shift;
+  my $mutation_point = rand( length( $chromosome ));
+  substr($chromosome, $mutation_point, 1,
+	 ( substr($chromosome, $mutation_point, 1) eq 1 )?0:1 );
+  return $chromosome;
+}
+
+sub crossover {
+  my ($chromosome_1, $chromosome_2) = @_;
+  my $length = length( $chromosome_1 );
+  my $xover_point_1 = int rand( $length - 2 );
+  my $range = 1 + int rand ( $length - $xover_point_1 );
+  my $swap_chrom = $chromosome_1;
+  substr($chromosome_1, $xover_point_1, $range,
+	 substr($chromosome_2, $xover_point_1, $range) );
+  substr($chromosome_2, $xover_point_1, $range,
+	 substr($swap_chrom, $xover_point_1, $range) );
+  return ( $chromosome_1, $chromosome_2 );
 }
 
 "010101"; # Magic true value required at end of module
@@ -36,7 +113,7 @@ This document describes Algorithm::Evolutionary::Simple version 0.0.3
 
 =head1 SYNOPSIS
 
-    use Algorithm::Evolutionary::Simple;
+    use Algorithm::Evolutionary::Simple qw(max_ones random_chromosome);
 
 =head1 DESCRIPTION
 
@@ -45,18 +122,34 @@ Assorted functions needed by an evolutionary algorithm app; just to get started
 
 =head1 INTERFACE 
 
-=head2 random_chromosome( length )
+=head2 random_chromosome( $length )
 
 Creates a binary chromosome, with uniform distribution of 0s and 1s,
 and returns it as a string.
 
+=head2 max_ones( $string )
+
+Classical function that returns the number of ones in a binary string.
+
+=head2 get_pool_roulette_wheel( $population_arrayref, $fitness_of_hashref, $how_many_I_need )
+
+Obtains a pool of new chromosomes using fitness_proportional selection
+
+=head2 produce_offspring( $population_hashref, $how_many_I_need )
+
+Uses mutation first and then crossover to obtain a new population
+
+=head2 mutate( $string )
+
+Bitflips a  a single point in the binary string
+
+=head2 crossover( $one_string, $another_string )
+
+Applies two-point crossover to both strings, returning them changed
+
 =head1 DIAGNOSTICS
 
-=for author to fill in:
-    List every single error and warning message that the module can
-    generate (even the ones that will "never happen"), with a full
-    explanation of each problem, one or more likely causes, and any
-    suggested remedies.
+None known
 
 =over
 
